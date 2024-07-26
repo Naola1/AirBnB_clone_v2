fe4d9a235a853998157340f6e37df5fd67d2ea24
naola1 <naolmitiku@gmail.com>
2024-07-26 16:36:22 +0300

    task-03

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Start web application with two routings
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_flask():
+    """Return string 'Hello HBNB!'
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Return string 'HBNB'
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Return reformatted text
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/<text>')
+@app.route('/python')
+def python_is_cool(text='cool'):
+    """Return reformatted text
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
