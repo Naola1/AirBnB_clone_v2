2daeaf2912fe6add6668d05395d45399ad38164c
naola1 <naolmitiku@gmail.com>
2024-07-26 16:14:26 +0300

    task-2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,10 +6,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
+
+@app.route('/')
 def hello():
-    """display a string 'Hello HBNB!'"""
-    return "Hello HBNB!"
+    """Return string when route queried
+    """
+    return 'Hello HBNB!'
 
 @app.route("/hbnb",strict_slashes=False)
 def hbnb():
