717704ba9ae941310bd1f6f3fa4c03010cb74769
naola1 <naolmitiku@gmail.com>
2024-07-26 17:08:45 +0300

    task-04

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Start web application with two routings
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_flask():
+    """Return string 'Hello HBNB!'
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    """Return string 'HBNB'
+    """
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Return reformatted text
+    """
+    return 'C ' + text.replace('_', ' ')
+
+@app.route('/python/<text>')
+@app.route('/python/')
+def python_is_cool(text='is cool'):
+    """Return reformatted text
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+@app.route('/number/<int:n>')
+def is_n_integer(n=None):
+    """returns if the parameter is an integer
+    """
+    return str(n) + ' is a number'
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
