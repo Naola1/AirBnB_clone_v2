17a9e37ec1c08ca253111e5475a57c778125ea30
naola1 <naolmitiku@gmail.com>
2024-07-26 15:47:55 +0300

    task-0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""script that starts flask web application frame work
- and listens on 0.0.0.0, port 5000"""
+"""Start a flask web app
+"""
 
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    """A function that returns a string 'Hello HBNB!'"""
-    return "Hello HBNB!"
 
-if __name__ == "__main__":
+@app.route('/')
+def hello_flask():
+    """Return string when route queried
+    """
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
     app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
