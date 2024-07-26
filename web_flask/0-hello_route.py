45c9c5861d0da20469ae88f3dcf580c0102cd358
naola1 <naolmitiku@gmail.com>
2024-07-26 15:23:28 +0300

    task-0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello():
+    """A function that returns a string 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port=5000,debug=None)
+    app.run(host='0.0.0.0', port=5000, debug=None)
