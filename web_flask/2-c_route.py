6ed73ada97388a361a6dd46de2a231bfce0c0abe
naola1 <naolmitiku@gmail.com>
2024-07-26 16:27:12 +0300

    task-2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
-"""script that starts flask web application frame work
- and listens on 0.0.0.0, port 5000"""
+"""Start web application with two routings
+"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    """Return string when route queried
+def hello_flask():
+    """Return string 'Hello HBNB!'
     """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hbnb():
-    """display a string 'HBNB'"""
+    """Return string 'HBNB'
+    """
     return 'HBNB'
 
+
 @app.route('/c/<text>')
-def c_test(text):
-    """display C followed by the value of the text variable"""
-    text=text.replace('_',' ')
-    return f"C {text}"
+def c_is_fun(text):
+    """Return reformatted text
+    """
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000,debug=None)
+    app.run(host='0.0.0.0', port=5000)
