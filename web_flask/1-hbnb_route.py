364a96e857af19694f520fb03fca4eb9d092e0e7
naola1 <naolmitiku@gmail.com>
2024-07-26 16:02:09 +0300

    task-01

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """starts flask web app
- """
+"""
 
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_flask():
     """Returns 'Hello HBNB!'"""
     return "Hello HBNB!"
 
-@app.route("/hbnb")
+@app.route('/hbnb')
 def hbnb():
     """Returns 'HBNB'"""
-    return "HBNB"
+    return 'HBNB'
 
 if __name__=="__main__":
     app.url_map.strict_slashes = False
