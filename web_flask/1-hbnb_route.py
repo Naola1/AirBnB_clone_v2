448ffd23dafb4ea85bc625efa0818d03e229a06f
naola1 <naolmitiku@gmail.com>
2024-07-26 16:12:13 +0300

    task-01

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""starts flask web app
+"""Start web application with two routings
 """
 
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_flask():
-    """Returns 'Hello HBNB!'
+    """Return string when route queried
     """
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
-    """Returns 'HBNB'
+    """Return string when route queried
     """
     return 'HBNB'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
