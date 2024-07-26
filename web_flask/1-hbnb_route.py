c4fb74e40055c550f0fcae0a0b02f70a4e0fa633
naola1 <naolmitiku@gmail.com>
2024-07-26 15:44:35 +0300

    task-01

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,18 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
 """script that starts flask web application frame work
- and listens on 0.0.0.0, port 5000"""
+ and listens on 0.0.0.0, port 5000
+ """
 
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
+@app.route("/")
 def hello():
+    """Returns 'Hello HBNB!'"""
     return "Hello HBNB!"
 
-@app.route("/hbnb",strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
+    """Returns 'HBNB'"""
     return "HBNB"
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port=5000,debug=None)
+    app.url_map.strict_slashes=False
+    app.run(host='0.0.0.0', port=5000)
