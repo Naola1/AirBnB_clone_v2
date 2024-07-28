279ef3bf0ac1edb625a000ccca313580c56c5167
naola1 <naolmitiku@gmail.com>
2024-07-28 07:37:04 +0300

    updated place

--- models/place.py
+++ models/place.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+
+from models.amenity import Amenity
+from models.review import Review
 from models.base_model import BaseModel, Base
 from models import storage_type
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.sql.schema import Table
 from sqlalchemy.orm import relationship
+
 
 if storage_type == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
                                  ForeignKey('places.id'),
-                                 primary_key=True, nullable=False),
+                                 primary_key=True,
+                                 nullable=False),
                           Column('amenity_id', String(60),
                                  ForeignKey('amenities.id'),
-                                 primary_key=True, nullable=False))
+                                 primary_key=True,
+                                 nullable=False)
+                          )
 
 
 class Place(BaseModel, Base):
@@ -23,22 +31,21 @@ class Place(BaseModel, Base):
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, default=0, nullable=False)
-        number_bathrooms = Column(Integer, default=0, nullable=False)
-        max_guest = Column(Integer, default=0, nullable=False)
-        price_by_night = Column(Integer, default=0, nullable=False)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         reviews = relationship('Review', backref='place',
                                cascade='all, delete, delete-orphan')
-        amenities = relationship('Amenity',
-                                 secondary='place_amenity',
-                                 viewonly=False)
+        amenities = relationship('Amenity', secondary=place_amenity,
+                                 viewonly=False, backref='place_amenities')
     else:
-        city_id = ''
-        user_id = ''
-        name = ''
-        description = ''
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
         number_rooms = 0
         number_bathrooms = 0
         max_guest = 0
@@ -47,11 +54,10 @@ class Place(BaseModel, Base):
         longitude = 0.0
         amenity_ids = []
 
-    if storage_type != 'db':
         @property
         def reviews(self):
             ''' returns list of review instances with place_id
-                equals to the current Place.id
+                equals to the cyrrent Place.id
                 FileStorage relationship between Place and Review
             '''
             from models import storage
@@ -69,11 +75,11 @@ class Place(BaseModel, Base):
                 contains all Amenity.id linked to the Place
             '''
             from models import storage
-            all_amenities = storage.all(Amenity)
+            all_amens = storage.all(Amenity)
             lst = []
-            for amenity in all_amenities.values():
-                if amenity.id in self.amenity_ids:
-                    lst.append(amenity)
+            for amen in all_amens.values():
+                if amen.id in self.amenity_ids:
+                    lst.append(amen)
             return lst
 
         @amenities.setter
@@ -82,5 +88,7 @@ class Place(BaseModel, Base):
                 attribute amenity_ids. accepts only Amenity
                 objects
             '''
-            if type(obj) == Amenity:
-                self.amenity_ids.append(obj.id)
+            if obj is not None:
+                if isinstance(obj, Amenity):
+                    if obj.id not in self.amenity_ids:
+                        self.amenity_ids.append(obj.id)
